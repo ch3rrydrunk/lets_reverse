ce5ef92f62feee0d1a27d7f36dafe236648e3012
caellis <serkhar@gmail.com>
2019-07-02 10:00:32 +0300

    done and done

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ from androguard.core.bytecodes.axml import ARSCParser
 ANDROID_SCHEME = "{http://schemas.android.com/apk/res/android}scheme"
 ANDROID_HOST = "{http://schemas.android.com/apk/res/android}host"
 ANDROID_NAME = "{http://schemas.android.com/apk/res/android}name"
+ANDROID_BACKUP = "{http://schemas.android.com/apk/res/android}allowBackup"
+ANDROID_CLEAR = "{http://schemas.android.com/apk/res/android}usesCleartextTraffic"
+ANDROID_VALUE = "{http://schemas.android.com/apk/res/android}value"
 
+# Данный скрипт принимает в качестве аргумента имя файла и осуществляет следующие операции:
 # 1. выводит значение флагов безопасности в Android Manifest 
 # - WebView Metrics 
-# 	какие такие метрики хэз;
+# 	<meta-data android:name="android.webkit.WebView.EnableSafeBrowsing" android:value="false" />
 # - usesCleartextTraffic
 #	[безопасно - android:usesCleartextTraffic=”false”];
 # - Backup
@@ -28,28 +32,51 @@ ANDROID_NAME = "{http://schemas.android.com/apk/res/android}name"
 #			</intent-filter>			
 # 3. выводит список библиотек, используемых в приложении.
 
+# Принимаем файл и задаем необходимые для работы переменные
 if len(sys.argv) != 2:
 	print("Я принимаю на вход только 1 параметр - путь к APK\n"
 			"И да, лучше кормить меня только APK, я не знаю, "
 			"что делать с другими файлами\n¯\\_(ツ)_/¯")
 	exit(400)
 file = sys.argv[1]
-
 apk = APK(file)
 axml = apk.get_android_manifest_xml()
-num_check = 0
+lil_pow = 0
+
 
 # 1. Флаги Безопасности
+print("Задание 1 - Флаги безопасности Manifest'а\n"
+		"а. Установки CleartextTraffic:")
+guess = axml.find("./application[@{}]".format(ANDROID_CLEAR))
+if guess != None:
+	print(guess.attrib.get(ANDROID_CLEAR))
+else:
+	print("Не обнаружено.")
+
+print("b. Установки allowBackup:")
+guess = axml.find("./application[@{}]".format(ANDROID_BACKUP))
+if guess != None:
+	print(guess.attrib.get(ANDROID_BACKUP))
+else:
+	print("Не обнаружено.")
+
+print("b. Установки WebView:")
+guess = axml.find("./application/meta-data[@{}='android.webkit.WebView.EnableSafeBrowsing']".format(ANDROID_NAME))
+if guess != None:
+	print(guess.attrib.get(ANDROID_VALUE))
+else:
+	print("Не обнаружено.")
 
 
 # 2. Список секретных кодов
 print("Задание 2 - Список секретных кодов")
 for element in axml.findall(".//receiver//data[@{}='android_secret_code']".format(ANDROID_SCHEME)):
-	num_check = 1
+	lil_pow = 1
 	print("Код {} вызывает {}".format(element.attrib.get(ANDROID_HOST), element.find(".../...").attrib.get(ANDROID_NAME)))
-if not num_check:
+if not lil_pow:
 	print("Не обнаружено.")
-num_check = 0
+lil_pow = 0
+
 
 # 3. Список библиотек
 print("Задание 3 - Список использованных библиотек:")
@@ -57,8 +84,7 @@ apk_dict = apk.files
 for pack in apk_dict:
 	sopack = str(pack)
 	if (sopack.endswith(".so")):
-		num_check = 1
+		lil_pow = 1
 		print(sopack.rsplit("/")[-1])
-if not num_check :
+if not lil_pow :
 	print("Не обнаружено.")
-num_check = 0
